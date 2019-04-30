7310130ef310f8e5ec021a99b9a45ce48dd64fa2
BeastMode00 <ktaoutel@gmail.com>
2019-04-30 08:42:38 -0400

    menu

--- src/tp2/ConfigGuide.java
+++ src/tp2/ConfigGuide.java
@@ -1,13 +1,17 @@
 package tp2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ConfigGuide {
 
+    Scanner entree = new Scanner(System.in);
+
     private final CorpsCeleste corpsCeleste = new CorpsCeleste();
 
     boolean boucle = true;
-    Scanner entree = new Scanner(System.in);
+
+    ArrayList<CorpsCeleste> liste = new ArrayList<>();
 
     protected void main() {
         recommencerProgramme();
@@ -39,13 +43,13 @@ public class ConfigGuide {
                 menu = Integer.parseInt(entree.nextLine());
                 System.out.println("");
                 while (menu < 1 || menu > 5) {
-                    System.out.println("Veuillez entrer un nombre entre 1 et 5:");
+                    System.out.println("Veuillez entrer un nombre entre 1 et 5: ");
                     menu = Integer.parseInt(entree.nextLine());
                     System.out.println("");
                 }
                 switch (menu) {
                     case 1:
-                        
+
                         break;
                     case 2:
                         assignerID();
@@ -65,7 +69,7 @@ public class ConfigGuide {
                 boucle = false;
             } catch (NumberFormatException e) {
                 System.out.println("");
-                System.out.println("Veuillez entrer un nombre entre 1 et 5:");
+                System.out.println("Veuillez entrer un nombre entre 1 et 5: ");
             }
         }
         boucle = true;
@@ -81,7 +85,7 @@ public class ConfigGuide {
         System.out.println("    2-  Planete gaseuse");
         System.out.println("    3-  Planete naine");
         System.out.println("    4-  Etoile");
-        System.out.println("    5-  Lune");
+        System.out.println("    5-  Satellite");
     }
 
     private void entrerTypeDAstre() {
@@ -92,35 +96,118 @@ public class ConfigGuide {
                 menu = Integer.parseInt(entree.nextLine());
                 System.out.println("");
                 while (menu < 1 || menu > 5) {
-                    System.out.println("Veuillez entrer un nombre entre 1 et 5:");
+                    System.out.println("Veuillez entrer un nombre entre 1 et 5: ");
                     menu = Integer.parseInt(entree.nextLine());
                     System.out.println("");
-                }switch (menu) {
+                }
+                switch (menu) {
                     case 1:
-                        
+                        entrerDonneesPlanetesTelluriques();
                         break;
                     case 2:
-                        
+                        entrerDonneesPlanetesGazeuses();
                         break;
                     case 3:
-
+                        entrerDonneesPlanetesNaines();
                         break;
                     case 4:
-
+                        entrerDonneesEtoiles();
                         break;
                     case 5:
-
+                        entrerDonneesSatellites();
                         break;
                 }
                 boucle = false;
             } catch (NumberFormatException e) {
                 System.out.println("");
-                System.out.println("Veuillez entrer un nombre entre 1 et 5:");
+                System.out.println("Veuillez entrer un nombre entre 1 et 5: ");
             }
         }
         boucle = true;
     }
+
+    private String entrerNomCC() {
+        String nom;
+        nom = entree.nextLine();
+        System.out.println("");
+        return nom;
+    }
+
+    private double entrerRayonCC() {
+        Double rayon = 0.0;
+        while (boucle) {
+            try {
+                rayon = Double.parseDouble(entree.nextLine());
+                System.out.println("");
+                boucle = false;
+                while (rayon <= 0) {
+                    System.out.println("Veuillez entrer un nombre entier positif: ");
+                    rayon = Double.parseDouble(entree.nextLine());
+                    System.out.println("");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("");
+                System.out.println("Veuillez entrer un nombre entier positif: ");
+            }
+        }
+        boucle = true;
+        return rayon;
+    }
     
-    
-    
+    private String entrerPlaneteLiee(){
+        String planeteLiee;
+        planeteLiee = entree.nextLine();
+        System.out.println("");
+        return planeteLiee;
+    }
+    private void entrerDonneesPlanetesTelluriques() {
+        CorpsCeleste planeteTellurique = new PlaneteTellurique();
+        liste.add(planeteTellurique);
+        System.out.print("Veuillez entrer le nom de la planeteTellurique decouverte: ");
+        planeteTellurique.setNom(entrerNomCC());
+        System.out.print("Veuillez entrer le rayon de la planeteTellurique decouverte (en km): ");
+        planeteTellurique.setRayon(entrerRayonCC());
+
+    }
+
+    private void entrerDonneesPlanetesGazeuses() {
+        CorpsCeleste planeteGazeuse = new PlaneteGazeuse();
+        liste.add(planeteGazeuse);
+        System.out.print("Veuillez entrer le nom de la planeteGazeuse decouverte: ");
+        planeteGazeuse.setNom(entrerNomCC());
+        System.out.print("Veuillez entrer le rayon de la planeteGazeuse decouverte (en km): ");
+        planeteGazeuse.setRayon(entrerRayonCC());
+    }
+
+    private void entrerDonneesPlanetesNaines() {
+        CorpsCeleste planeteNaine1 = new PlaneteNaine();
+        PlaneteNaine planeteNaine2 = new PlaneteNaine();
+        liste.add(planeteNaine1);
+        System.out.print("Veuillez entrer le nom de la planeteNaine decouverte: ");
+        planeteNaine1.setNom(entrerNomCC());
+        System.out.print("Veuillez entrer le rayon de la planeteNaine decouverte (en km): ");
+        planeteNaine1.setRayon(entrerRayonCC());
+    }
+
+    private void entrerDonneesEtoiles() {
+        CorpsCeleste etoile = new Etoile();
+        liste.add(etoile);
+        System.out.print("Veuillez entrer le nom de l etoile decouverte: ");
+        etoile.setNom(entrerNomCC());
+        System.out.print("Veuillez entrer le rayon de l etoile decouverte (en km): ");
+        etoile.setRayon(entrerRayonCC());
+    }
+
+    private void entrerDonneesSatellites() {
+        CorpsCeleste satellite1 = new Satellites();
+        Satellites satellite2 = new Satellites();
+        liste.add(satellite1);
+        System.out.print("Veuillez entrer le nom du satellite decouvert: ");
+        satellite1.setNom(entrerNomCC());
+        System.out.print("Veuillez entrer le rayon du satellite decouvert (en km): ");
+        satellite1.setRayon(entrerRayonCC());
+        System.out.print("Veuillez entrer la planète liée au satellite decouvert: ");
+        satellite2.setPlaneteLiee(entrerPlaneteLiee());
+    }
+
 }
--- src/tp2/Etoile.java
+++ src/tp2/Etoile.java
@@ -5,11 +5,11 @@ public class Etoile extends CorpsCeleste {
     private int stade;
     private double masse;
     private String planetesLiees;
-    
-    public Etoile(int ID,String nom,double Rayon,int stade,double masse,String planetesLiees){
+//    
+//    public Etoile(int ID,String nom,double Rayon,int stade,double masse,String planetesLiees){
 //        super(ID,nom,Rayon);
-        this.masse = masse;
-        this.planetesLiees = planetesLiees;
-        this.stade = stade;
-    }
+//        this.masse = masse;
+//        this.planetesLiees = planetesLiees;
+//        this.stade = stade;
+//    }
 }
