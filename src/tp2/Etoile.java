d06e3473d42a4e3d43e01e7a9864f25324f9da3c
BeastMode00 <ktaoutel@gmail.com>
2019-05-01 16:27:25 -0400

    dasd

--- src/tp2/Etoile.java
+++ src/tp2/Etoile.java
@@ -1,15 +1,15 @@
 package tp2;
 
 public class Etoile extends CorpsCeleste {
-    
-    private int stade;
+
+    private int phase;
     private double masse;
-    private String planetesLiees;
-    
-    public Etoile(int ID,String nom,double Rayon,int stade,double masse,String planetesLiees){
-        super(ID,nom,Rayon);
+    private String [] planetesLiees;
+
+    public Etoile(int ID, String nom, double Rayon, int phase, double masse, String[] planetesLiees) {
+        super(ID, nom, Rayon);
         this.masse = masse;
-        this.planetesLiees = planetesLiees;
-        this.stade = stade;
+//        this.[]planetesLiees = planetesLiees[];
+        this.phase = phase;
     }
 }
